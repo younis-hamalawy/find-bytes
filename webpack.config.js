b7934e85515cb8b18537beb15ffb1305998a53e1
Younis Hamalawy <younis.hamalawy@gmail.com>
2017-10-07 23:01:41 -0700

    initial commit/ boiler plate/ install react-redux/ webpack / dependencies

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var path = require('path');
+let webpack = require('webpack');
+
+module.exports = {
+  context: __dirname,
+  entry: './index.jsx',
+  output: {
+    path: path.resolve(__dirname),
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react'],
+        },
+      },
+    ],
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*'],
+  },
+};
