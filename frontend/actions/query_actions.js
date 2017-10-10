64933a91467a3a4da4e95f3deac592d9f223403a
Younis Hamalawy <younis.hamalawy@gmail.com>
2017-10-09 18:14:04 -0700

    import query_pi_util into query actions, create errors functions

--- frontend/actions/query_actions.js
+++ frontend/actions/query_actions.js
@@ -1,10 +1,26 @@
-
+import * as QueryAPIUtil from '../util/query_api_util';
 
 export const RECEIVE_QUERY = 'RECEIVE_QUERY';
+export const RECEIVE_ERRORS = 'RECEIVE_ERRORS';
+export const CLEAR_ERRORS = 'CLEAR_ERRORS';
 
-export const receiveQuery = query => {
-  return {
-    type: RECEIVE_QUERY,
-    query,
-  };
-};
+export const receiveQuery = query => ({
+  type: RECEIVE_QUERY,
+  query
+});
+
+export const receiveErrors = errors => ({
+  type: RECEIVE_ERRORS,
+  errors
+});
+
+export const clearErrors = () => ({
+  type: CLEAR_ERRORS
+});
+
+export const fetchQueryResults = query => dispatch => (
+  QueryAPIUtil.fetchQueryResults(query).then(responce => (
+      dispatch(receiveQuery(responce));
+      dispatch(clearErrors())),
+    err => dispatch(receiveErrors(err)))
+);
