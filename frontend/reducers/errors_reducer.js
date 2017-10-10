34f171d3b94445404ca8f34ffb8dd4e145a28899
Younis Hamalawy <younis.hamalawy@gmail.com>
2017-10-09 18:21:09 -0700

    create and configure errors_reducer

---
+++ frontend/reducers/errors_reducer.js
@@ -0,0 +1,26 @@
+import merge from 'lodash/merge';
+import { RECEIVE_ERRORS, CLEAR_ERRORS } from '../actions/query_actions';
+
+const defaultState = {
+  query: {},
+  errors: [],
+};
+
+const errorsReducer = (state = defaultState, action) => {
+  Object.freeze(state);
+
+  switch (action.type) {
+    case RECEIVE_ERRORS:
+      return merge({}, state, {
+        errors: action.errors,
+      });
+    case CLEAR_ERRORS:
+      return merge({}, state, {
+        errors: [],
+      });
+    default:
+      return state;
+  }
+};
+
+export default errorsReducer;
