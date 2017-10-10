925c1c02f4e6cef70f5e458c0c12552a36fcccce
Younis Hamalawy <younis.hamalawy@gmail.com>
2017-10-10 13:09:12 -0700

    add map function to main.jsx

--- frontend/components/main/main.jsx
+++ frontend/components/main/main.jsx
@@ -10,6 +10,20 @@ class Main extends React.Component {
     };
   }
 
+  componentDidMount() {
+    const location = navigator.geolocation;
+
+    location.getCurrentPosition(position => {
+      this.setState({ latitude: position.coords.latitude });
+      this.setState({ longitude: position.coords.longitude });
+
+      const map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: position.coords.latitude, lng: position.coords.longitude },
+        zoom: 14,
+      });
+    });
+  }
+
   render() {
     return (
       <div>
