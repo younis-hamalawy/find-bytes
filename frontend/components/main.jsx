e4ed5dbf3514c4323527944daa173cef49bee16e
Younis Hamalawy <younis.hamalawy@gmail.com>
2017-10-27 12:59:04 -0700

    drop down window intial css

--- frontend/components/main.jsx
+++ frontend/components/main.jsx
@@ -5,11 +5,8 @@ class Main extends React.Component {
     super(props);
     this.state = {
       map: '',
-      location: '',
-      latitude: '',
-      longitude: '',
       markers: [],
-      input: '',
+      service: '',
       strictBounds: '',
       marker: '',
       infowindow: '',
@@ -22,6 +19,8 @@ class Main extends React.Component {
     this.addResult = this.addResult.bind(this);
     this.dropMarker = this.dropMarker.bind(this);
     this.clearMarkers = this.clearMarkers.bind(this);
+    this.clearResults = this.clearResults.bind(this);
+    this.showInfoWindow = this.showInfoWindow.bind(this);
   }
 
   componentDidMount() {
@@ -68,12 +67,14 @@ class Main extends React.Component {
           // type: ['restaurant']
           bounds: map.getBounds()
         };
-        var service = new google.maps.places.PlacesService(map);
-        service.nearbySearch(request, callback);
+        that.service = new google.maps.places.PlacesService(map);
+        that.service.nearbySearch(request, callback);
         let then = that;
 
         function callback(results, status) {
           if (status == google.maps.places.PlacesServiceStatus.OK) {
+            then.clearResults();
+            then.clearMarkers();
             // Create a marker for each hotel found, and
             // assign a letter of the alphabetic to each marker icon.
             for (var i = 0; i < results.length; i++) {
@@ -88,7 +89,7 @@ class Main extends React.Component {
               // If the user clicks a hotel marker, show the details of that hotel
               // in an info window.
               then.state.markers[i].placeResult = results[i];
-              google.maps.event.addListener(then.state.markers[i], 'click', then.showInfoWindow);
+              google.maps.event.addListener(then.state.markers[i], 'click', then.showInfoWindow(i));
               setTimeout(then.dropMarker(i), i * 100);
               then.addResult(results[i], i);
             }
@@ -137,8 +138,8 @@ class Main extends React.Component {
   }
 
   clearMarkers() {
-    for (var i = 0; i < markers.length; i++) {
-      if (markers[i]) {
+    for (var i = 0; i < this.state.markers.length; i++) {
+      if (this.state.markers[i]) {
         this.state.markers[i].setMap(null);
       }
     }
@@ -172,6 +173,78 @@ class Main extends React.Component {
     tr.appendChild(iconTd);
     tr.appendChild(nameTd);
     results.appendChild(tr);
+  }
+
+  clearResults() {
+    var results = document.getElementById('results');
+    while (results.childNodes[0]) {
+      results.removeChild(results.childNodes[0]);
+    }
+  }
+
+  // Get the place details for a hotel. Show the information in an info window,
+  // anchored on the marker for the hotel that the user selected.
+  showInfoWindow(i) {
+    var then = this;
+    // console.log(marker.service.getDetails)
+    this.service.getDetails({placeId: this.state.markers[i].placeResult.place_id},
+      function(place, status) {
+        if (status !== google.maps.places.PlacesServiceStatus.OK) {
+          return;
+        }
+        infoWindow.open(map, then.state.markers[i]);
+        then.buildIWContent(place);
+      });
+  }
+
+  // Load the place information into the HTML elements used by the info window.
+  buildIWContent(place) {
+    document.getElementById('iw-icon').innerHTML = '<img class="hotelIcon" ' +
+        'src="' + place.icon + '"/>';
+    document.getElementById('iw-url').innerHTML = '<b><a href="' + place.url +
+        '">' + place.name + '</a></b>';
+    document.getElementById('iw-address').textContent = place.vicinity;
+
+    if (place.formatted_phone_number) {
+      document.getElementById('iw-phone-row').style.display = '';
+      document.getElementById('iw-phone').textContent =
+          place.formatted_phone_number;
+    } else {
+      document.getElementById('iw-phone-row').style.display = 'none';
+    }
+
+    // Assign a five-star rating to the hotel, using a black star ('&#10029;')
+    // to indicate the rating the hotel has earned, and a white star ('&#10025;')
+    // for the rating points not achieved.
+    if (place.rating) {
+      var ratingHtml = '';
+      for (var i = 0; i < 5; i++) {
+        if (place.rating < (i + 0.5)) {
+          ratingHtml += '&#10025;';
+        } else {
+          ratingHtml += '&#10029;';
+        }
+      document.getElementById('iw-rating-row').style.display = '';
+      document.getElementById('iw-rating').innerHTML = ratingHtml;
+      }
+    } else {
+      document.getElementById('iw-rating-row').style.display = 'none';
+    }
+
+    // The regexp isolates the first part of the URL (domain plus subdomain)
+    // to give a short URL for displaying in the info window.
+    if (place.website) {
+      var fullUrl = place.website;
+      var website = this.state.hostnameRegexp.exec(place.website);
+      if (website === null) {
+        website = 'http://' + place.website + '/';
+        fullUrl = website;
+      }
+      document.getElementById('iw-website-row').style.display = '';
+      document.getElementById('iw-website').textContent = website;
+    } else {
+      document.getElementById('iw-website-row').style.display = 'none';
+    }
   }
 
   // onPlaceChanged() {
@@ -208,10 +281,36 @@ class Main extends React.Component {
             </table>
           </div>
         </div>
-        <div id="infowindow-content">
+        <div id="infocontent">
           <img src="" width={16} height={16} id="place-icon" />
           <span id="place-name" className="title" /><br />
           <span id="place-address" />
+        </div>
+        <div id="info">
+          <div id="info-content">
+            <table>
+              <tr id="iw-url-row" className="iw_table_row">
+                <td id="iw-icon" className="iw_table_icon"></td>
+                <td id="iw-url"></td>
+              </tr>
+              <tr id="iw-address-row" className="iw_table_row">
+                <td className="iw_attribute_name">Address:</td>
+                <td id="iw-address"></td>
+              </tr>
+              <tr id="iw-phone-row" className="iw_table_row">
+                <td className="iw_attribute_name">Telephone:</td>
+                <td id="iw-phone"></td>
+              </tr>
+              <tr id="iw-rating-row" className="iw_table_row">
+                <td className="iw_attribute_name">Rating:</td>
+                <td id="iw-rating"></td>
+              </tr>
+              <tr id="iw-website-row" className="iw_table_row">
+                <td className="iw_attribute_name">Website:</td>
+                <td id="iw-website"></td>
+              </tr>
+            </table>
+          </div>
         </div>
       </div>
     );
